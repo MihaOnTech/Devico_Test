5ac662b8786242b570694fdc24394640d798a20f
MIHA DEVOPS <mihaontech@gmail.com>
2024-05-06 20:59:30 +0200

    Add Web3 Metamask Provider

--- src/components/providers/Web3Provider.js
+++ src/components/providers/Web3Provider.js
@@ -6,40 +6,41 @@ const Web3Context = createContext(null);
 export const Web3Provider = ({ children }) => {
     const [web3, setWeb3] = useState(null);
     const [accounts, setAccounts] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        const loadWeb3 = async () => {
-            if (window.ethereum) {
+    const connectMetaMask = async () => {
+        if (window.ethereum) {
+            setIsLoading(true);
+            try {
+                console.log("Connect Metamask")
+                await window.ethereum.request({ method: 'eth_requestAccounts' });
                 const web3Instance = new Web3(window.ethereum);
+                console.log("Web3 Instance: ", web3Instance)
+                const accounts = await web3Instance.eth.getAccounts();
+                console.log("Accounts: ", accounts);
                 setWeb3(web3Instance);
-                try {
-                    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-                    setAccounts(accounts);
-                } catch (error) {
-                    console.error('Error accessing accounts:', error);
-                }
-            } else {
-                console.error('MetaMask is not installed!');
+                setAccounts(accounts);
+            } catch (error) {
+                console.error('Error accessing accounts:', error);
             }
             setIsLoading(false);
-        };
-
-        loadWeb3();
-    }, []);
+        } else {
+            console.error('MetaMask is not installed!');
+        }
+    };
 
     useEffect(() => {
-        window.ethereum.on('accountsChanged', (accounts) => {
+        window.ethereum?.on('accountsChanged', (accounts) => {
             setAccounts(accounts);
         });
 
-        window.ethereum.on('chainChanged', (_) => {
+        window.ethereum?.on('chainChanged', () => {
             window.location.reload();
         });
     }, []);
 
     return (
-        <Web3Context.Provider value={{ web3, accounts, isLoading }}>
+        <Web3Context.Provider value={{ web3, accounts, isLoading, connectMetaMask }}>
             {children}
         </Web3Context.Provider>
     );
